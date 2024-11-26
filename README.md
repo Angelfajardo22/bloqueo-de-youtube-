03ce84279d4fc01cae8740da2caa7bb441eca2ca
Angelfajardo22 <fabianfajardo740@gmail.com>
2024-11-25 22:27:59 -0500

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# bloqueo-de-youtube-
-youtube.com
---
+++ README.py
@@ -0,0 +1,10 @@
+# bloqueo-de-youtube-
+1 www.youtube.com
+
+2 httppps://yotube.com
+
+3 httppps://m.yotube.com
+
+4 httppps://.yotube.com/
+
+5 https://www.youtube.com/
